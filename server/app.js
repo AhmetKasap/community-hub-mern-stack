180c58cac2b658216b41398718136522854ab7c4
Ahmet Kasap <aahmetkasap@gmail.com>
2023-08-09 17:10:58 +0300

    startt

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+require('dotenv').config()
+
+app.get('/', (req,res) => {
+    res.send('server running...')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("Server listening...")
+    
+})
+
